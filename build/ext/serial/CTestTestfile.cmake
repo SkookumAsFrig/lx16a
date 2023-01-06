d4a316d3be09ef0c52b48f4c3f8e71f73fd024be
K5200 <kss223@cornell.edu>
2023-01-05 18:15:00 -0800

    removed contents of build

--- build/ext/serial/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/k5200/lx16a/ext/serial
-# Build directory: /home/k5200/lx16a/build/ext/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../gtest")
-subdirs("tests")
--- build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/k5200/lx16a/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
