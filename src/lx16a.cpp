b503872cd108575164a8f65f0159770b12ae0694
K5200 <kss223@cornell.edu>
2023-01-06 21:17:36 -0800

    began class construction, verified serial library functionality

--- src/lx16a.cpp
+++ src/lx16a.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include "serial/serial.h"
 
+class lx16a_chain
+{
+    private:
+        unsigned char *servo_id;
+        port;
+        alias;
+        char *temp;
+        int *position;
+        int *motor_power;
+        
+    public:
+        lx16a_chain ( );
+        void setmode();
+        void 
+        void unload();
+        int* get_temps();
+}
+
+void enumerate_ports()
+{
+    std::vector<serial::PortInfo> devices_found = serial::list_ports();
+    std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+    while( iter != devices_found.end() )
+    {
+        serial::PortInfo device = *iter++;
+
+        printf( "(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
+            device.hardware_id.c_str() );
+    }
+}
 
 int main()
 {
     std::cout<<"Hello World"<<std::endl;
+    enumerate_ports();
 
     return 0;
 }
