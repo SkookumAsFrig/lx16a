f89463840841d49d4af9868f77a266224e14095f
K5200 <kss223@cornell.edu>
2023-01-12 14:49:10 -0800

    pass port stuct by reference experiment success

--- tests/test.cpp
+++ tests/test.cpp
@@ -15,38 +15,26 @@ class lx16a_chain
         unsigned int ser_delay;
         
     public:
-        ser_port port_st;
-        lx16a_chain(const std::string &portname, unsigned int baud, unsigned int timeout = 0, unsigned int id = 0)
+        ser_port* port_st;
+        lx16a_chain(ser_port* port_add)
         {
-            port_st.group_id = id;
-            port_st.port.setPort(portname);
-            port_st.port.setBaudrate(baud);
-            serial::Timeout chain_time= serial::Timeout::simpleTimeout(timeout);
-            std::cout<<chain_time.inter_byte_timeout<<std::endl;
-            port_st.port.setTimeout(chain_time);
-            port_st.port.open();
-            // serial::Serial new_port(portname, baud, serial::Timeout::simpleTimeout(timeout));
-            // std::cout << &new_port << std::endl;
-            // port_st.port = &new_port;
-            // std::cout<<"Print Port"<<std::endl;
-            // std::cout<<port_st.port->getPort()<<std::endl;
-            // std::cout<<new_port.getPort()<<std::endl;
+            port_st = port_add;
         }
 
         std::string query_port()
         {
-            return port_st.port.getPort();
+            return port_st->port.getPort();
         }
 
         void query_open(){
-            if(port_st.port.isOpen())
+            if(port_st->port.isOpen())
                 std::cout << " Yes." << std::endl;
             else
                 std::cout << " No." << std::endl;
         }
 
         unsigned int query_timeout(){
-            return port_st.port.getTimeout().inter_byte_timeout;
+            return port_st->port.getTimeout().inter_byte_timeout;
         }
         // void setmode();
         // void 
@@ -93,18 +81,29 @@ int main()
 
     // std::cout<<"Hello World"<<std::endl;
 
-    lx16a_chain testtest("/dev/ttyUSB0", 115200, 1000, 1);
+    
+
+    ser_port port_struct;
+    port_struct.group_id = 1;
+    port_struct.port.setPort("/dev/ttyUSB0");
+    port_struct.port.setBaudrate(115200);
+    serial::Timeout chain_time= serial::Timeout::simpleTimeout(1000);
+    std::cout<<chain_time.inter_byte_timeout<<std::endl;
+    port_struct.port.setTimeout(chain_time);
+    port_struct.port.open();
+
+    lx16a_chain testtest(&port_struct);
 
     std::cout<<"Hello World"<<std::endl;
     // std::cout<<testtest.port_st.port<<std::endl;
     std::cout << "Is the serial port open?"<<std::endl;
-    if(testtest.port_st.port.isOpen())
+    if(testtest.port_st->port.isOpen())
         std::cout << " Yes." << std::endl;
     else
         std::cout << " No." << std::endl;
 
     std::cout<<"Print Baud"<<std::endl;
-    std::cout<<testtest.port_st.port.getBaudrate()<<std::endl;
+    std::cout<<testtest.port_st->port.getBaudrate()<<std::endl;
 
     std::cout<<"Print Private Port"<<std::endl;
     std::cout<<testtest.query_port()<<std::endl;
