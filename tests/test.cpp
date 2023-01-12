f9ea70eaeab9b3eca205a1844d58ac1061d9bd11
K5200 <kss223@cornell.edu>
2023-01-12 11:58:06 -0800

    stuct serial obj pointer appears to be cause of bad alloc and seg fault, fixed struct to direct store serial obj, need to redesign interaction model for sharing serial port

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "serial/serial.h"
-#include <string>
+
+struct ser_port
+{
+    unsigned int group_id;
+    serial::Serial port;
+};
 
 class lx16a_chain
 {
@@ -10,18 +15,38 @@ class lx16a_chain
         unsigned int ser_delay;
         
     public:
-        struct ser_port
+        ser_port port_st;
+        lx16a_chain(const std::string &portname, unsigned int baud, unsigned int timeout = 0, unsigned int id = 0)
         {
-            unsigned int group_id;
-            serial::Serial* port;
-        } port_st;
-
-        lx16a_chain(const std::string &portname, unsigned int baud, unsigned int timeout, unsigned int id = 0)
-        {
-            ser_port port_st;
             port_st.group_id = id;
-            serial::Serial new_port(portname, baud, serial::Timeout::simpleTimeout(timeout));
-            port_st.port = &new_port;
+            port_st.port.setPort(portname);
+            port_st.port.setBaudrate(baud);
+            serial::Timeout chain_time= serial::Timeout::simpleTimeout(timeout);
+            std::cout<<chain_time.inter_byte_timeout<<std::endl;
+            port_st.port.setTimeout(chain_time);
+            port_st.port.open();
+            // serial::Serial new_port(portname, baud, serial::Timeout::simpleTimeout(timeout));
+            // std::cout << &new_port << std::endl;
+            // port_st.port = &new_port;
+            // std::cout<<"Print Port"<<std::endl;
+            // std::cout<<port_st.port->getPort()<<std::endl;
+            // std::cout<<new_port.getPort()<<std::endl;
+        }
+
+        std::string query_port()
+        {
+            return port_st.port.getPort();
+        }
+
+        void query_open(){
+            if(port_st.port.isOpen())
+                std::cout << " Yes." << std::endl;
+            else
+                std::cout << " No." << std::endl;
+        }
+
+        unsigned int query_timeout(){
+            return port_st.port.getTimeout().inter_byte_timeout;
         }
         // void setmode();
         // void 
@@ -54,17 +79,41 @@ int main()
     std::cout<<"Hello World"<<std::endl;
     enumerate_ports();
 
+    // serial::Serial haha("/dev/ttyUSB0", 115200, serial::Timeout::simpleTimeout(500));
+    // serial::Serial* news_port = &haha;
+    // std::cout<<news_port->getPort()<<std::endl;
+
+    // std::cout<<"Hello World"<<std::endl;
+    // std::cout << "Is the serial port open?"<<std::endl;
+    // // if(testtest.port_st.port->isOpen())
+    // if(news_port->isOpen())
+    //     std::cout << " Yes." << std::endl;
+    // else
+    //     std::cout << " No." << std::endl;
+
+    // std::cout<<"Hello World"<<std::endl;
+
     lx16a_chain testtest("/dev/ttyUSB0", 115200, 1000, 1);
+
     std::cout<<"Hello World"<<std::endl;
-    // serial::Serial news_port("/dev/ttyUSB1", 5400, serial::Timeout::simpleTimeout(500));
-    // std::cout<<news_port.getPort()<<std::endl;
-    std::cout<<"Hello World"<<std::endl;
-    std::cout << "Is the serial port open?";
-    if(testtest.port_st.port->isOpen())
+    // std::cout<<testtest.port_st.port<<std::endl;
+    std::cout << "Is the serial port open?"<<std::endl;
+    if(testtest.port_st.port.isOpen())
         std::cout << " Yes." << std::endl;
     else
         std::cout << " No." << std::endl;
-    std::cout<<testtest.port_st.port->getPort()<<std::endl;
+
+    std::cout<<"Print Baud"<<std::endl;
+    std::cout<<testtest.port_st.port.getBaudrate()<<std::endl;
+
+    std::cout<<"Print Private Port"<<std::endl;
+    std::cout<<testtest.query_port()<<std::endl;
+
+    std::cout<<"Check Open Port"<<std::endl;
+    testtest.query_open();
+
+    std::cout<<"Print Timeout"<<std::endl;
+    std::cout<<testtest.query_timeout()<<std::endl;
 
     return 0;
 }
