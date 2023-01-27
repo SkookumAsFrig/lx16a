6976ad6f954c8c809bf0cf1edd8316bc73f06b98
K5200 <kss223@cornell.edu>
2023-01-27 01:22:28 -0800

    fixed googletest seg fault by pulling 1.8.2 branch and changing cmake git automation

--- tests/unit/unit_test.cpp
+++ tests/unit/unit_test.cpp
@@ -1,8 +1,16 @@
 #include "gtest/gtest.h"
 #include "lx16a.hpp"
 
-TEST(FuncUnitTests, UnloadTest){
-    EXPECT_EQ(1, 2) << "1 and 2 should return failed test";
+int Factorial(int n){
+  if(n>1) return n*Factorial(n-1);
+  else return n;
+}
+
+TEST(FactorialTest, Positive) {
+  EXPECT_EQ(1, Factorial(1));
+  EXPECT_EQ(2, Factorial(2));
+  EXPECT_EQ(6, Factorial(3));
+  EXPECT_EQ(40320, Factorial(8));
 }
 
 int main(int argc, char **argv) {
